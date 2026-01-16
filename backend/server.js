4b5b0513d3dbf33c7643fccb0375c240eed464cb
Niv Levy <140189588+NivLevy-gh@users.noreply.github.com>
2026-01-16 00:46:49 -0500

    ports

--- backend/server.js
+++ backend/server.js
@@ -658,3 +658,9 @@ let summaryObj = await generateCandidateSummaryJSON({
 // =============================
 // START SERVER
 // =============================
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
